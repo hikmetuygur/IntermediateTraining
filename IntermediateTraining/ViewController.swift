667164ccf530ef6390defba3fefc132ec48048f1
hikmetuygur <uygurhikmet@gmail.com>
2022-11-29 16:42:46 +0300

    Stylizing User Interface
    Made changes AppDelegate, SceneDelegate and ViewController

--- IntermediateTraining/ViewController.swift
+++ IntermediateTraining/ViewController.swift
@@ -12,9 +12,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .yellow
+        view.backgroundColor = .white
+        
+        navigationItem.title = "Companies"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "plus.circle"), style: .plain, target: self, action: #selector(handleAddCompany))
+        
+        setupNavigationStyle()
     }
-
-
+    
+    @objc func handleAddCompany() {
+        print("Adding company..")
+    }
+    
+    func setupNavigationStyle() {
+        navigationController?.navigationBar.isTranslucent = false
+        
+        let lightRed = UIColor(red: 247/255, green: 66/255, blue: 82/255, alpha: 1)
+        
+        navigationController?.navigationBar.barTintColor = lightRed
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+    }
 }
-

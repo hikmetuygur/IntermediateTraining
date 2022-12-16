9bf843b8caa98c3f38f9dd2ddc13eb7ac1d73417
hikmetuygur <uygurhikmet@gmail.com>
2022-12-16 16:20:48 +0300

    Implement photo selector function

--- IntermediateTraining/View/CreateCompanyViewController.swift
+++ IntermediateTraining/View/CreateCompanyViewController.swift
@@ -13,7 +13,7 @@ protocol CreateNewCompanyDelegate {
     func didEditCompany(company: Company)
 }
 
-class CreateCompanyViewController: UIViewController {
+class CreateCompanyViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     //MARK: - Variables
 
@@ -22,17 +22,30 @@ class CreateCompanyViewController: UIViewController {
     var company: Company? {
         didSet {
             nameTextField.text = company?.name
-            
-            guard let founded = company?.founded else {return}
+            guard let founded = company?.founded else { return }
             datePicker.date = founded
         }
     }
 
+    lazy var companyImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(systemName: "photo.circle"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleSelectPhoto)))
+        return imageView
+
+    }()
+
+    let selectImageButton: UIButton = {
+        let imageButton = UIButton()
+        imageButton.translatesAutoresizingMaskIntoConstraints = false
+
+        return imageButton
+    }()
 
     let nameLabel: UILabel = {
         let label = UILabel()
         label.text = "Name"
-
         //enable AutoLayout
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
@@ -62,7 +75,7 @@ class CreateCompanyViewController: UIViewController {
         setupUI()
 
         view.backgroundColor = UIColor(hexString: "#D6E4E5")
-        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "chevron.backward"), style: .plain, target: self, action: #selector(handleCancel))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancel))
 
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(handleSave))
         navigationItem.leftBarButtonItem?.tintColor = .white
@@ -78,6 +91,7 @@ class CreateCompanyViewController: UIViewController {
     }
 
     private func setupUI() {
+
         let backgroundView = UIView()
         backgroundView.backgroundColor = UIColor(hexString: "#FEBE8C")
         backgroundView.translatesAutoresizingMaskIntoConstraints = false
@@ -88,8 +102,15 @@ class CreateCompanyViewController: UIViewController {
         backgroundView.heightAnchor.constraint(equalToConstant: 250).isActive = true
         backgroundView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 50)
 
+        view.addSubview(companyImageView)
+        companyImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 8).isActive = true
+        companyImageView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        companyImageView.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        companyImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+
+
         view.addSubview(nameLabel)
-        nameLabel.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        nameLabel.topAnchor.constraint(equalTo: companyImageView.bottomAnchor).isActive = true
         nameLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16).isActive = true
         nameLabel.widthAnchor.constraint(equalToConstant: 100).isActive = true
         nameLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
@@ -101,7 +122,7 @@ class CreateCompanyViewController: UIViewController {
         nameTextField.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
         nameTextField.bottomAnchor.constraint(equalTo: nameLabel.bottomAnchor).isActive = true
         nameTextField.topAnchor.constraint(equalTo: nameLabel.topAnchor).isActive = true
-        
+
         view.addSubview(datePicker)
         datePicker.topAnchor.constraint(equalTo: nameLabel.bottomAnchor).isActive = true
         datePicker.bottomAnchor.constraint(equalTo: backgroundView.bottomAnchor).isActive = true
@@ -122,7 +143,31 @@ class CreateCompanyViewController: UIViewController {
         } else {
             editFunctionSave()
         }
+    }
 
+    @objc private func handleSelectPhoto() {
+        print("Select Photo")
+
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        present(imagePickerController, animated: true, completion: nil)
+    }
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        print(info)
+
+        if let editedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            companyImageView.image = editedImage
+        } else if let originalImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            companyImageView.image = originalImage
+        }
+
+        dismiss(animated: true, completion: nil)
     }
 
     private func createFunctionSave() {
